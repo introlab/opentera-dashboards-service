a38b8c7ec69d6ca83df0e931906d9dac9d90cd68
Dominic Létourneau <doumdi@gmail.com>
2024-01-17 15:04:34 -0500

    Base implementation

---
+++ Globals.py
@@ -0,0 +1,14 @@
+from ConfigManager import ConfigManager
+
+
+# Configuration manager
+config_man = ConfigManager()
+
+# Database manager
+# db_man = DBManager()
+
+# Redis client & keys
+redis_client = None
+
+# Service
+service = None
