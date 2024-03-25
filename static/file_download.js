0ece58ef2ab18a72773c4a10af9b478b12ae3f86
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2024-03-25 16:52:46 -0400

    Refs #2, working download. Wrong name.

--- static/file_download.js
+++ static/file_download.js
@@ -1,8 +1,47 @@
 function fileDownloadFromBrowser(jsonDocumentString)
 {
+    /*
+    jsonDocumentString: JSON string
+    Format:
+    {
+        "url": "full url with params",
+        "method": "GET/POST",
+        "headers":  Object with key-value pairs
+    }
+    */
+
+
     try {
         //Convert string to JSON object
         var jsonDocument = JSON.parse(jsonDocumentString);
+
+        //Create AJAX request with the given method, url and headers
+        var xhr = new XMLHttpRequest();
+        xhr.open(jsonDocument.method, jsonDocument.url, true); //async
+        for (var key in jsonDocument.headers) {
+            xhr.setRequestHeader(key, jsonDocument.headers[key]);
+        }
+        xhr.responseType = 'blob';
+        
+        //On load, download the file
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var contentType = xhr.getResponseHeader('Content-Type');
+                var blob = new Blob([xhr.response], { type: contentType });
+                var url = URL.createObjectURL(blob);
+                var a = document.createElement('a');
+                a.href = url;
+                a.download = "downloaded_file";
+                a.click();
+                window.URL.revokeObjectURL(url);
+            }
+            else {
+                console.error("fileDownloadFromBrowser() failed with", xhr.status);
+            }
+        };
+
+        //Send the request
+        xhr.send();
 
         // do something
         console.log("fileDownloadFromBrowser() called with", jsonDocument);
