b07ba3d6b8f60d2c7201450cbc4897d6362964bc
Simon Briere <simon.briere@usherbrooke.ca>
2024-04-11 14:03:12 -0400

    Refs #2. Added OpenTera logo to loading screen in WebAssembly

--- Frontend/DashboardsViewer/src/app_environment.h
+++ Frontend/DashboardsViewer/src/app_environment.h
@@ -7,10 +7,10 @@
 
 void set_qt_environment()
 {
-    qputenv("QT_IM_MODULE", "qtvirtualkeyboard");
-    qputenv("QT_VIRTUALKEYBOARD_DESKTOP_DISABLE", "1");
+   /* qputenv("QT_IM_MODULE", "qtvirtualkeyboard");
+    qputenv("QT_VIRTUALKEYBOARD_DESKTOP_DISABLE", "1");*/
     qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1");
-    qputenv("QT_ENABLE_HIGHDPI_SCALING", "0");
+    qputenv("QT_ENABLE_HIGHDPI_SCALING", "1");
     qputenv("QT_LOGGING_RULES", "qt.qml.connections=false");
     qputenv("QT_QUICK_CONTROLS_CONF", ":/qtquickcontrols2.conf");
     qputenv("QML_COMPAT_RESOLVE_URLS_ON_ASSIGNMENT", "1");
