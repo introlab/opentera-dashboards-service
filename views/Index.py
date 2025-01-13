a15f8c5ef1fc7bb0b20c30dfdb840c6e117d19a4
Dominic Létourneau <doumdi@gmail.com>
2025-01-13 13:54:13 -0500

    Refs #1, testing login page

--- views/Index.py
+++ views/Index.py
@@ -31,6 +31,31 @@ class Index(MethodView):
         if not os.path.exists('static/DashboardsViewerApp.html'):
             return flask_app.send_static_file('default_index.html')
 
+        return render_template('login.html',
+                                backend_hostname=quote(backend_hostname),
+                                backend_port=quote(backend_port))
+
+    def post(self):
+        # Do something about the post data which is json format
+        login_information = request.json
+
+        backend_hostname = self.flaskModule.config.backend_config['hostname']
+        backend_port = self.flaskModule.config.backend_config['port']
+
+        # Look for variables set in NGINX reverse proxy...
+        if 'X_EXTERNALSERVER' in request.headers:
+            backend_hostname = request.headers['X_EXTERNALSERVER']
+
+        if 'X_EXTERNALPORT' in request.headers:
+            backend_port = request.headers['X_EXTERNALPORT']
+
+        user_name = 'Anonymous'
+
+        # Verify if static/DashboardsViewerApp.html exists
+        # send default index.html if not
+        if not os.path.exists('static/DashboardsViewerApp.html'):
+            return flask_app.send_static_file('default_index.html')
+
         return render_template('dashboards.html',
                                 backend_hostname=quote(backend_hostname),
                                 backend_port=quote(backend_port),
